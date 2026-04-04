b513a7c72c3ce16017162e2c257d4114a7e328f0
Autumn Pan <pan662607@gmail.com>
2026-04-04 00:12:18 -0400

    [feat]: add scattered point sorting and __repr__

--- timescalelib/core.py
+++ timescalelib/core.py
@@ -33,10 +33,13 @@ class timescale:
     simplified_intervals.append(current_interval) # Don't forget to add the last interval
     self.intervals = simplified_intervals
 
-    # Next, we remove scattered points that are within intervals
+    # Next, we sort the scattered points
+    self.scattered_points.sort()
+    # Finally, we remove scattered points that are within intervals
     self.scattered_points = [point for point in self.scattered_points if not any(interval.start <= point <= interval.end for interval in self.intervals)]
   
-
+  def __repr__(self):
+    return f"timescale(intervals={self.intervals}, scattered_points={self.scattered_points})"
         
 
     
