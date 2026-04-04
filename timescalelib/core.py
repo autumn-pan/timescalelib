233e9d7711e2474bd27bf8e6d70c31905bc88d85
Autumn Pan <pan662607@gmail.com>
2026-04-04 00:25:57 -0400

    [feat]: add extremal time scale functions
    added min and max functions for time scales

--- timescalelib/core.py
+++ timescalelib/core.py
@@ -35,11 +35,24 @@ class timescale:
 
     # Next, we sort the scattered points
     self.scattered_points.sort()
+    # Then, we remove duplicates from the scattered points
+    self.scattered_points = list(set(self.scattered_points))
     # Finally, we remove scattered points that are within intervals
     self.scattered_points = [point for point in self.scattered_points if not any(interval.start <= point <= interval.end for interval in self.intervals)]
   
+  def max(self):
+    # The maximum of the timescale is the maximum of the end points of the intervals and the maximum of the scattered points
+    max_interval = max(interval.end for interval in self.intervals) if self.intervals else float('-inf')
+    max_scattered = max(self.scattered_points) if self.scattered_points else float('-inf')
+    return max(max_interval, max_scattered)
+  
+  def min(self):
+    # The minimum of the timescale is the minimum of the start points of the intervals and the minimum of the scattered points
+    min_interval = min(interval.start for interval in self.intervals) if self.intervals else float('inf')
+    min_scattered = min(self.scattered_points) if self.scattered_points else float('inf')
+    return min(min_interval, min_scattered)
+
+  
   def __repr__(self):
     return f"timescale(intervals={self.intervals}, scattered_points={self.scattered_points})"
-        
-
-    
+  
